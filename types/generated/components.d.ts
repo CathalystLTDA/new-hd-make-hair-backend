5351d4397576e2a86c61adf9b202818049974044
eric-cidade <ericmathias2012@gmail.com>
2024-09-23 15:15:37 -0300

    adding new collections

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface MaterialConstrucao extends Schema.Component {
+  collectionName: 'components_material_construcaos';
+  info: {
+    displayName: 'Construcao';
+    icon: 'user';
+  };
+  attributes: {
+    Imagem: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    Tipo: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'material.construcao': MaterialConstrucao;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,44 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiProdutoProduto extends Schema.CollectionType {
+  collectionName: 'produtos';
+  info: {
+    singularName: 'produto';
+    pluralName: 'produtos';
+    displayName: 'Produto';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Nome: Attribute.String;
+    Descricao: Attribute.Text;
+    Calor: Attribute.Boolean;
+    URL: Attribute.String;
+    Lacy: Attribute.Boolean;
+    Manutencao: Attribute.Text;
+    Comprimento: Attribute.Integer;
+    TipoDeConstrucao: Attribute.Component<'material.construcao'>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::produto.produto',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::produto.produto',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -798,6 +836,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::produto.produto': ApiProdutoProduto;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
