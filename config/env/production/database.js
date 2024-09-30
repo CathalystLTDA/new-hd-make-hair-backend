41cf6a28fd1755a2eb653c90fd077b61c8839b7e
Guilherme Zago <contatogzii@gmail.com>
2024-09-30 13:34:05 -0300

    try again

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,24 +1,11 @@
- // path: /config/env/production/database.ts
- export default ({ env }) => ({
-   connection: {
-     client: 'postgres',
-     connection: {
-       host: "ep-wild-poetry-a404wid3.us-east-1.aws.neon.tech",
-       port: 5432,
-       database: "verceldb",
-       user: "default",
-       password: "PtgBL5qHn9NS",
-       ssl: {
-         rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-       },
-     },
-     debug: true,
-   },
- });
+const parse = require("pg-connection-string").parse;
 
+// const { host, port, database, user, password } = parse(
+//    process.env.POSTGRES_URL
+//  );
 
-//  DATABASE_HOST=ep-wild-poetry-a404wid3.us-east-1.aws.neon.tech
-// DATABASE_PORT=5432
-// DATABASE_NAME=verceldb
-// DATABASE_USERNAME=default
-// DATABASE_PASSWORD=PtgBL5qHn9NS
+export default ({ env }) => ({
+  connection: {
+    connectionString:"postgres://default:PtgBL5qHn9NS@ep-wild-poetry-a404wid3.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  },
+});
