6f549265e11e9495d6819dd4a660b9ce8aa56e64
Guilherme Zago <contatogzii@gmail.com>
2024-09-30 14:29:53 -0300

    followinbg docs

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,5 +4,9 @@ export default ({ env }) => ({
      url: env('APP_URL'), // Sets the public URL of the application.
      app: {
        keys: env.array('APP_KEYS')
-     },
+  },
+
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
  });
