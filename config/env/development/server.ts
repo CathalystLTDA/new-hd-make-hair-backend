2fca89bc6914a98b8dddf5afcb8eb962910c2e43
Guilherme Zago <contatogzii@gmail.com>
2024-09-30 15:47:41 -0300

    changes for deploy

--- config/env/development/server.ts
+++ config/env/development/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ["g/nS+XTBDFlL0p7vpONB3g==","gVS5uncNzmm2Bmjml0G+Zg==","6NIFFFnAHRczrDTvKySR9g==","obzNIttGzSzOcdAe2TQXOw=="])
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
--- config/server.ts
+++ config/server.ts
@@ -3,6 +3,6 @@ export default ({ env }) => ({
      proxy: true,
      url: env('APP_URL'), // Sets the public URL of the application.
      app: {
-       keys: env.array('APP_KEYS')
+       keys: env.array('APP_KEYS', ["g/nS+XTBDFlL0p7vpONB3g==","gVS5uncNzmm2Bmjml0G+Zg==","6NIFFFnAHRczrDTvKySR9g==","obzNIttGzSzOcdAe2TQXOw=="])
      },
  });
