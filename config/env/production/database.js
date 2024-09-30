dffad8dfc329d361a58daa97bac17c8a75701870
Guilherme Zago <contatogzii@gmail.com>
2024-09-30 13:36:16 -0300

    try again part2

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,6 +6,7 @@ const parse = require("pg-connection-string").parse;
 
 export default ({ env }) => ({
   connection: {
-    connectionString:"postgres://default:PtgBL5qHn9NS@ep-wild-poetry-a404wid3.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
-  },
+    connectionString: env.DATABASE_URL ||
+      "postgres://default:PtgBL5qHn9NS@ep-wild-poetry-a404wid3.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+},
 });
