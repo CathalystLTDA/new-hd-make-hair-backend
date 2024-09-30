1f568df81881af975f009d3e0534d52effecadf2
Guilherme Zago <contatogzii@gmail.com>
2024-09-30 14:57:23 -0300

    new approach

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,8 @@
 // path: /config/env/production/server.ts
 export default ({ env }) => ({
-     host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+     proxy: true,
+     url: env('APP_URL'), // Sets the public URL of the application.
+     app: {
+       keys: env.array('APP_KEYS')
+     },
  });
