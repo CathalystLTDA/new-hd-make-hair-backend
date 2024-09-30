bbe6a4a47a2cf71fcb50ff5b1b2cba532026ee6f
Guilherme Zago <contatogzii@gmail.com>
2024-09-30 15:57:22 -0300

    up

--- config/env/production/server.ts
+++
@@ -1,8 +0,0 @@
-// path: /config/env/production/server.ts
-export default ({ env }) => ({
-     proxy: true,
-     url: env('APP_URL'), // Sets the public URL of the application.
-     app: {
-       keys: env.array('APP_KEYS', ["g/nS+XTBDFlL0p7vpONB3g==","gVS5uncNzmm2Bmjml0G+Zg==","6NIFFFnAHRczrDTvKySR9g==","obzNIttGzSzOcdAe2TQXOw=="])
-     },
- });
