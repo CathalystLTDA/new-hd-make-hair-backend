1676fc95fb53ec552fe789b395528e69316f893b
Guilherme Zago <contatogzii@gmail.com>
2024-10-24 19:15:02 -0300

    fodase2

--- config/env/development/plugins.ts
+++ config/env/development/plugins.ts
@@ -12,7 +12,7 @@ upload: {
           },
           region: 'us-east-1',
           params: {
-            // ACL: env('AWS_ACL', 'public-read'),
+            ACL: env('AWS_ACL', 'public-read'),
             // signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
             Bucket: 'hdmh-strapi-s3-bucket',
           },
